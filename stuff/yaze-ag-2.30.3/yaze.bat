6ab4fd34114326b62e25825d803989582cdc5f8c
Tom Seddon <tom@tmbp>
2015-04-21 22:57:20 +0100

    Add yaze-ag-2.30.3 verbatim.

---
+++ stuff/yaze-ag-2.30.3/yaze.bat
@@ -0,0 +1,2 @@
+.\mintty.exe --size 80,25 -o BoldAsFont=no -o FontHeight=16 -o CursorType=block -o CursorBlinks=no -o CursorColour=0,255,0 -o BellSound=no -o BellFlash=yes --exec ./yaze_bin.exe
+exit
