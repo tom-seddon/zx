a19867b7b6b0826c6eeb055c2073007760f6f842
Tom Seddon <tom@tmbp>
2015-04-23 21:32:53 +0100

    Cobble together an SDL-based Spectrum emulator with keyboard input.

--- stuff/qnd_emu/main.cpp
+++ stuff/qnd_emu/main.cpp
@@ -6,18 +6,17 @@
 //////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////
 
-#include <ncurses.h>
 #include <stdlib.h>
 #include <locale.h>
 #include <stdio.h>
 #include <vector>
 #include "Z80.h"
+#include <SDL.h>
 
 //////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////
 
 static uint8_t g_z80_mem[65536];
-
 
 struct LogEntry
 {
@@ -33,15 +32,47 @@ static std::vector<LogEntry> g_log_entries;
 
 static bool g_quit;
 
-enum ColourPairs
+static const char ROM_FILE_NAME[]="48.rom";
+
+//////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////
+
+struct SpectrumKey
 {
-    SPECTRUM_COLOURS=1,
-    DEBUG_COLOURS,
+    SDL_Scancode pc_scan_code;
+    int row,col;
 };
 
-static WINDOW *g_swin,*g_dwin;
+#define S(X) SDL_SCANCODE_##X
 
-static const char ROM_FILE_NAME[]="48.rom";
+static uint8_t g_spectrum_keys_up[8];//[row]
+
+static const SpectrumKey g_spectrum_keys_table[]={
+    {S(LSHIFT),0,0},{S(Z),0,1},{S(X),0,2},{S(C),0,3},{S(V),0,4},
+    {S(A),1,0},{S(S),1,1},{S(D),1,2},{S(F),1,3},{S(G),1,4},
+    {S(Q),2,0},{S(W),2,1},{S(E),2,2},{S(R),2,3},{S(T),2,4},
+    {S(1),3,0},{S(2),3,1},{S(3),3,2},{S(4),3,3},{S(5),3,4},
+    {S(0),4,0},{S(9),4,1},{S(8),4,2},{S(7),4,3},{S(6),4,4},
+    {S(P),5,0},{S(O),5,1},{S(I),5,2},{S(U),5,3},{S(Y),5,4},
+    {S(RETURN),6,0},{S(L),6,1},{S(K),6,2},{S(J),6,3},{S(H),6,4},
+    {S(SPACE),7,0},{S(RSHIFT),7,1},{S(M),7,2},{S(N),7,3},{S(B),7,4},
+    {SDL_SCANCODE_UNKNOWN}
+};
+
+#undef S
+
+static const SpectrumKey *g_spectrum_keys[SDL_NUM_SCANCODES];
+
+//////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////
+
+static void InitKeys()
+{
+    for(const SpectrumKey *sk=g_spectrum_keys_table;sk->pc_scan_code!=SDL_SCANCODE_UNKNOWN;++sk)
+	g_spectrum_keys[sk->pc_scan_code]=sk;
+
+    memset(g_spectrum_keys_up,255,sizeof g_spectrum_keys_up);
+}
 
 //////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////
@@ -65,145 +96,32 @@ static void LoadROM()
     f=0;
 }
 
-//////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////
-
-static void DoEndwin()
+static void RedrawScreen(SDL_Texture *texture)
 {
-    endwin();
-}
-
-//////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////
-
-static void DeleteWindow(WINDOW **w)
-{
-    if(*w)
-    {
-	delwin(*w);
-	*w=0;
-    }
-}
-
-static void DeleteWindows()
-{
-    DeleteWindow(&g_swin);
-    DeleteWindow(&g_dwin);
-}
-
-//////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////
-
-// static void wcls(WINDOW *win)
-// {
-//     int w,h;
-//     getmaxyx(win,h,w);
-
-//     //scrollok(win,FALSE);
-
-//     for(int i=0;i<w*h;++i)
-//     {
-// 	//wprintw(win,"\xe2\x96\x98");
-// 	wprintw(win,"FART");
-//     }
-
-//     //scrollok(win,TRUE);
-
-//     //wmove(win,0,0);
-//     //wclear(win);
-// }
-
-//////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////
-
-static void UpdateWindows()
-{
-    DeleteWindows();
-
-    int w,h;
-    getmaxyx(stdscr,h,w);
-
-    if(w<128||h<110)
-    {
-    	fprintf(stderr,"FATAL: screen must be at least 128x110.\n");
-    	exit(1);
-    }
-
-    g_swin=newwin(96,w,0,0);
-    g_dwin=newwin(h-96,w,96,0);
-
-    scrollok(g_dwin,TRUE);
-    scrollok(g_swin,FALSE);
-
-    wattrset(g_swin,COLOR_PAIR(SPECTRUM_COLOURS));
-    wattrset(g_dwin,COLOR_PAIR(DEBUG_COLOURS));
-
-    wprintw(g_dwin,"Hello.\n");
-
-    wrefresh(g_dwin);
-    wrefresh(g_swin);
-}
-
-//////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////
-
-static const char *const BLOCK_CHARS[]={
-    " ",
-    "\xe2\x96\x9d",//1
-    "\xe2\x96\x98",//2
-    "\xe2\x96\x80",//3
-    "\xe2\x96\x97",//4
-    "\xe2\x96\x90",//5
-    "\xe2\x96\x9a",//6
-    "\xe2\x96\x9c",//7
-    "\xe2\x96\x96",//8
-    "\xe2\x96\x9e",//9
-    "\xe2\x96\x8c",//10
-    "\xe2\x96\x9b",//11
-    "\xe2\x96\x84",//12
-    "\xe2\x96\x9f",//13
-    "\xe2\x96\x99",//14
-    "\xe2\x96\x88",//15
-};
-
-static void RedrawScreen(int n)
-{
-    for(int y=0;y<192;y+=2)
+    uint8_t pixels[192][256];
+    
+    for(int y=0;y<192;++y)
     {
 	uint16_t addr=0x4000+((y&7)<<8)+(((y>>3)&7)<<5)+((y>>6)<<11);
 	
-	const uint8_t *as=&g_z80_mem[addr+0x0000];
-	const uint8_t *bs=&g_z80_mem[addr+0x0100];
-	    
-	wmove(g_swin,y/2,0);
+	uint8_t *p=pixels[y];
 	
 	for(int x=0;x<32;++x)
 	{
-	    uint8_t a=as[x];
-	    uint8_t b=bs[x];
+	    uint v=g_z80_mem[addr+x];
 
-	    uint8_t a76=a>>6,a54=(a>>4)&3,a32=(a>>2)&3,a10=a&3;
-	    uint8_t b76=b>>6,b54=(b>>4)&3,b32=(b>>2)&3,b10=b&3;
-
-
-	    uint8_t c0=(a76<<0)|(b76<<2);
-	    uint8_t c1=(a54<<0)|(b54<<2);
-	    uint8_t c2=(a32<<0)|(b32<<2);
-	    uint8_t c3=(a10<<0)|(b10<<2);
-
-	    wprintw(g_swin,"%s%s%s%s",BLOCK_CHARS[c0],BLOCK_CHARS[c1],BLOCK_CHARS[c2],BLOCK_CHARS[c3]);
-	    //wprintw(g_swin,"%04X",g_z80_state.PC.W);
+	    *p++=(v&0x80)?255:0;
+	    *p++=(v&0x40)?255:0;
+	    *p++=(v&0x20)?255:0;
+	    *p++=(v&0x10)?255:0;
+	    *p++=(v&0x08)?255:0;
+	    *p++=(v&0x04)?255:0;
+	    *p++=(v&0x02)?255:0;
+	    *p++=(v&0x01)?255:0;
 	}
-
-	if(y==0)
-	    wprintw(g_swin," %d",n);
-	else if(y==2)
-	    wprintw(g_swin," %zu",g_log_entries.size());
-	
-	//wprintw(g_swin," %d\n",n);
     }
 
-    wrefresh(g_swin);
+    SDL_UpdateTexture(texture,nullptr,pixels,sizeof pixels[0]);
 }
 
 //////////////////////////////////////////////////////////////////////////
@@ -211,6 +129,9 @@ static void RedrawScreen(int n)
 
 static void LogZ80State(const Z80 *z)
 {
+    if(!g_log_file)
+	return;
+	
     // f a c b e d l h
     // f' a' c' b' e' d' l' h'
     // IXl IXh IYl IYh SPl SPh PCl PCh
@@ -287,8 +208,7 @@ byte RdZ80(register word Addr)
 
 void OutZ80(register word Port,register byte Value)
 {
-    wprintw(g_dwin,"OUT: Port=%04X Value=%02X\n",Port,Value);
-    wrefresh(g_dwin);
+    //printf("OUT: Port=%04X Value=%02X\n",Port,Value);
 }
 
 //////////////////////////////////////////////////////////////////////////
@@ -296,10 +216,31 @@ void OutZ80(register word Port,register byte Value)
 
 byte InZ80(register word Port)
 {
-    wprintw(g_dwin,"IN: Port=%04X\n",Port);
-    wrefresh(g_dwin);
+    uint8_t val=255;
     
-    return 255;
+    if((Port&0xff)==0xfe)
+    {
+	uint8_t sel=Port>>8;
+	
+	for(int row=0;row<8;++row)
+	{
+	    if(!(sel&(1<<row)))
+		val&=g_spectrum_keys_up[row];
+	}
+	
+	// FE 1111 1110
+	// FD 1111 1101
+	// FB 1111 1011
+	// F7 1111 0111
+	// EF 1110 1111
+	// DF 1101 1111
+	// BF 1011 1111
+	// 7F 0111 1111
+    }
+
+    //printf("IN: Port=%04X: Result=%02X\n",Port,val);
+    
+    return val;
 }
 //////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////
@@ -335,74 +276,135 @@ byte DebugZ80(register Z80 *R)
 //////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////
 
-int main()
+static void UpdateKeyFlag(const SDL_Keysym &k,bool pressed)
+{
+    const SpectrumKey *sk=g_spectrum_keys[k.scancode];
+    if(!sk)
+	return;
+
+    uint8_t *v=&g_spectrum_keys_up[sk->row];
+
+    uint8_t mask=1<<sk->col;
+
+    bool changed=false;
+    if(pressed)
+    {
+	if(*v&mask)
+	    changed=true;
+	
+	*v&=~mask;
+    }
+    else
+    {
+	if(!(*v&mask))
+	    changed=true;
+	
+	*v|=mask;
+    }
+
+    // if(changed)
+    // 	printf("%s: new state: %s\n",SDL_GetScancodeName(k.scancode),pressed?"DOWN":"UP");
+    
+}
+
+static bool HandleSDLEvents()
+{
+    SDL_Event e;
+    while(SDL_PollEvent(&e))
+    {
+	switch(e.type)
+	{
+	case SDL_KEYDOWN:
+	case SDL_KEYUP:
+	    {
+		UpdateKeyFlag(e.key.keysym,e.key.state==SDL_PRESSED);
+	    }
+	    break;
+
+	case SDL_QUIT:
+	    return false;
+
+	case SDL_WINDOWEVENT:
+	    {
+	    }
+	    break;
+	}
+    }
+
+    return true;
+}
+
+int main(int argc,char *argv[])
 {
     LoadROM();
-    
-    setlocale(LC_ALL,"");
-    initscr();
-    atexit(&DoEndwin);
+    InitKeys();
 
-    g_log_file=fopen("z80_log.dat","wb");
+    SDL_Init(SDL_INIT_VIDEO|SDL_INIT_TIMER);
+
+    SDL_Window *main_window=SDL_CreateWindow("Q'n'D Emu",
+					     SDL_WINDOWPOS_UNDEFINED,SDL_WINDOWPOS_UNDEFINED,
+					     512,384,
+					     SDL_WINDOW_RESIZABLE);
+    
+
+    //SDL_Surface *main_window_surface=SDL_GetWindowSurface(main_window);
+
+    SDL_Renderer *renderer=SDL_CreateRenderer(main_window,-1,0);
+
+    SDL_Texture *back_buffer=SDL_CreateTexture(renderer,
+					       SDL_PIXELFORMAT_RGB332,
+					       SDL_TEXTUREACCESS_STREAMING,
+					       256,192);
+
+    //g_log_file=fopen("z80_log.dat","wb");
     atexit(&CloseLogFile);
 
     if(g_log_file)
 	fputc(1,g_log_file);	// version
     
-    start_color();
-    noecho();
-
-    init_pair(DEBUG_COLOURS,COLOR_YELLOW,COLOR_BLUE);
-    init_pair(SPECTRUM_COLOURS,COLOR_WHITE,COLOR_BLACK);
-
-    UpdateWindows();
-
     Z80 z80_state;
     memset(&z80_state,0,sizeof z80_state);
     ResetZ80(&z80_state);
     z80_state.Trace=1;
 
-    int num_redraws=0;
-    int num_instrs=0;
-    bool step=false;
+    // int num_redraws=0;
+    // int num_instrs=0;
+    // bool step=false;
+
+    uint64_t ticks_per_frame=SDL_GetPerformanceFrequency()/50;
+    uint64_t ticks_per_ms=SDL_GetPerformanceFrequency()/1000;
     
-    for(;;)
+    while(HandleSDLEvents())
     {
-	ExecZ80(&z80_state,1);
-
-	bool redraw=false;
+	uint64_t next_vsync=SDL_GetPerformanceCounter()+ticks_per_frame;
 	
-	if(num_instrs++%10000==0)
-	{
-	    redraw=true;
-	    //IntZ80(&z80_state,255);
-	}
+	for(int i=0;i<10000;++i)
+	    ExecZ80(&z80_state,1);
 
-	if(step)
-	    redraw=true;
+	IntZ80(&z80_state,255);
 
-	if(redraw)
-	{
-	    RedrawScreen(num_redraws++);
-	    FlushLog();
-	}
+	RedrawScreen(back_buffer);
 
-	if(num_instrs>1000000)//z80_state.PC.W==0x120a)
-	{
-	    break;
-	}
+	SDL_RenderCopy(renderer,back_buffer,nullptr,nullptr);
+	
+	SDL_RenderPresent(renderer);
 
-	if(step)
+	FlushLog();
+
+	// make the emulated Spectrum run at ~50Hz without mashing the
+	// CPU...
+	for(;;)
 	{
-	    const Z80 *z=&z80_state;
-	    
-	    wprintw(g_dwin,"%04X : AF =%04X BC =%04X DE =%04X HL =%04X  IX=%04X  PC=%04X\n",
-		    z->PC.W,z->AF.W,z->BC.W,z->DE.W,z->HL.W,z->IX.W,z->PC.W);
-	    wprintw(g_dwin,"       AF'=%04X BC'=%04X DE'=%04X HL'=%04X  IY=%04X  SP=%04X\n",
-		    z->AF1.W,z->BC1.W,z->DE1.W,z->HL1.W,z->IY.W,z->SP.W);
-	    
-	    wrefresh(g_dwin);
-	    wgetch(g_dwin);
+	    uint64_t now=SDL_GetPerformanceCounter();
+	    if(now>=next_vsync)
+		break;
+
+	    if(next_vsync-now>2*ticks_per_ms)
+		SDL_Delay(1);
 	}
     }
+
+    SDL_Quit();
+
+    return 0;
 }
